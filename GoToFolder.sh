c07eb135011eebe90d3e6c5446023a3be9460305
2Manchu <42014390+2Manchu@users.noreply.github.com>
2022-01-08 14:11:10 -0600

    Fix infinite loop in script
    Fixed infinite loops until the user enters a valid path in the path entry box. The script will now terminate if the user closes the path entry box.

--- GoToFolder.sh
+++ GoToFolder.sh
@@ -7,6 +7,13 @@ fileUtility=nautilus
 # Function to prompt user for directory
 getPath() {
 	userPath=$(zenity --entry --width 250 --title "Go to Folder..." --text "Path to Folder")
+
+	# Make sure the user has actually entered a path and not just closed the box. -z option checks for an empty variable
+	if [[ -z $userPath ]]
+	then
+		exit
+	fi
+
 	# Expand the '~' in the specified path
 	userPath="${userPath//\~/$HOME}"
 }
@@ -16,7 +23,7 @@ getPath
 # If the user inputs an directory that doesn't exist, error and prompt for a new path
 while [ ! -d "$userPath" ]
 do
-	zenity --error --width 300 --title "Invalid directory specified" --text "$userPath does not exist, or is not a directory."
+	zenity --error --width 300 --title "Invalid directory specified" --text "Path \"$userPath\" does not exist, or is not a directory."
 	getPath
 done
 
--- README.md
+++ README.md
@@ -20,6 +20,4 @@ Your desktop environment must support GTK+ as this application uses Zenity to pr
 Edit the script to change the variable `fileUtility` to your file manager of choice. By default this is Nautilus as it is the included GNOME file manager, for which the script was originally designed for. Give the script executable permissions with `sudo chmod a+x GoToFolder.sh` and execute via your terminal. Alternatively, you can assign it a hotkey through your DE so you can launch it on command.
 
 # Issues
-* Script infinite loops when prompting user for a path if they simply close the dialog
 * Make dialog boxes scale properly to user's display resolution
-
